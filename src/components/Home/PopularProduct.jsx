8d397d115af2930c0e9af2d1164614f0d061bfc9
rahiji9 <aliahmedrahi007@gmail.com>
2025-01-02 21:14:57 +0600

    deploy in vercel

--- src/components/Home/PopularProduct.jsx
+++ src/components/Home/PopularProduct.jsx
@@ -10,16 +10,22 @@ const PopularProduct = () => {
       .get("https://coffee-store-server-flame-two.vercel.app/coffee")
       .then((data) => setCoffees(data.data));
   }, []);
+
+  if (!coffees) {
+    return (
+      <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-default-600"></div>
+    );
+  }
   return (
     <div>
       <div className="text-2xl lg:text-4xl text-amber-950 font-bold text-center lg:mt-20 mt-10">
         <h2>Our Popular Products</h2>
-        <p>
+        <div>
           <p className="text-xs lg:text-sm font-normal mt-3 lg:mb-10 mb-5">
             We are an online coffee shop offering high-quality, affordable
             coffee <br /> blends to customers worldwide.
           </p>
-        </p>
+        </div>
 
         {/* <div className="border-b-2 w-1/3 mx-auto mb-5 mt-2 border-amber-950"></div> */}
       </div>
